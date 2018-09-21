440b4903be89c4482d09e92b54f80c25b0343cdb
Gustavo Lopes <p000gulopes@prservicos.com.br>
2018-09-21 19:23:38 -0300

    Tests browsers' automatic copy APIs

--- src/app/pages/home/main/table/table.component.ts
+++ src/app/pages/home/main/table/table.component.ts
@@ -1,11 +1,49 @@
 import { Component, Input } from '@angular/core';
+import clipboard from "clipboard-polyfill";
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+	selector: 'app-table',
+	templateUrl: './table.component.html',
+	styleUrls: ['./table.component.scss']
 })
 export class TableComponent {
 	@Input('title') title: string;
 	@Input('condition') condition: boolean;
+
+	copy(id: string) {
+		// console.log(clipboard);
+
+		// const el = document.getElementById(id) as Node;
+		const str = document.getElementById(id).innerHTML;
+		/*
+		let range;
+		let sel;
+
+		if (document.createRange && window.getSelection) {
+			range = document.createRange();
+			sel = window.getSelection();
+			sel.removeAllRanges();
+			try {
+				range.selectNodeContents(el);
+				sel.addRange(range);
+			} catch (e) {
+				range.selectNode(el);
+				sel.addRange(range);
+			}
+			document.execCommand("Copy");
+			sel.removeAllRanges();
+		}
+*/
+
+			function listener(e) {
+				console.log(str);
+
+				e.clipboardData.setData("text/html", str);
+				// e.clipboardData.setData("text/plain", str);
+				e.preventDefault();
+			}
+			document.addEventListener("copy", listener);
+			document.execCommand("copy");
+			document.removeEventListener("copy", listener);
+	}
 }
