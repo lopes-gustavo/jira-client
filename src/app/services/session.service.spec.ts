66be40aafa538253805ca4b290c9bc6bc4076b2b
Gustavo Lopes <lopes.gustavojf@gmail.com>
2018-10-07 19:26:42 -0300

    Create tests for SessionService

--- src/app/services/session.service.spec.ts
+++ src/app/services/session.service.spec.ts
@@ -1,15 +1,25 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { SessionService } from './session.service';
+import { author } from '../_helper/mocks/jira';
 
 describe('SessionService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [SessionService]
-    });
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [SessionService]
+		});
+	});
 
-  it('should be created', inject([SessionService], (service: SessionService) => {
-    expect(service).toBeTruthy();
-  }));
+	it('should be created', inject([SessionService], (service: SessionService) => {
+		expect(service).toBeTruthy();
+	}));
+
+	it('should gather information from session/local storage at initialization', inject([SessionService], (service: SessionService) => {
+		sessionStorage.setItem('currentUser', JSON.stringify(author));
+		expect(service.currentUser).toBeNull();
+
+		service.ngOnInit();
+
+		expect(service.currentUser).toEqual(author);
+	}));
 });
