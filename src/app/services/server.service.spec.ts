99bc0f53545999065c56e2c11cb118d09b65f008
Gustavo Lopes <lopes.gustavojf@gmail.com>
2018-10-07 19:25:06 -0300

    Create tests for ServerService

--- src/app/services/server.service.spec.ts
+++ src/app/services/server.service.spec.ts
@@ -1,15 +1,51 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 import { ServerService } from './server.service';
+import { author, searchResponse } from '../_helper/mocks/jira';
 
 describe('ServerService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ServerService]
-    });
-  });
+	const httpClient = {get: jasmine.createSpy('get')};
 
-  it('should be created', inject([ServerService], (service: ServerService) => {
-    expect(service).toBeTruthy();
-  }));
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [
+				ServerService,
+				{provide: HttpClient, useValue: httpClient}
+			]
+		});
+	});
+
+	it('should be created', inject([ServerService], (service: ServerService) => {
+		expect(service).toBeTruthy();
+	}));
+
+	it('#login should return a Jira.Author', inject([ServerService], (service: ServerService) => {
+		httpClient.get.and.returnValue(of(author));
+
+		service.login('token', 'server').subscribe(
+			(value => expect(value).toEqual(author)),
+			(error => fail(error))
+		);
+	}));
+
+	it('#getJiraData should return a Jira.SearchResponse', inject([ServerService], (service: ServerService) => {
+		httpClient.get.and.returnValue(of(searchResponse));
+
+		service.getJiraData('token', 'server', 'name', '2018-10-08', '2018-10-10').subscribe(
+			(value => expect(value).toEqual(searchResponse)),
+			(error => fail(error))
+		);
+	}));
+
+	it('#getTeamJiraData should return a Jira.SearchResponse', inject([ServerService], (service: ServerService) => {
+		httpClient.get.and.returnValue(of(searchResponse));
+
+		service.getTeamJiraData('token', 'server', '2018-10-08', '2018-10-10').subscribe(
+			(value => expect(value).toEqual(searchResponse)),
+			(error => fail(error))
+		);
+	}));
+
 });
