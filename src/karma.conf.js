7f75dc470933aeb952982faf62288b62b53fc36f
Gustavo Lopes <lopes.gustavojf@gmail.com>
2018-10-07 23:34:50 -0300

    Add junit to npm test script

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -5,30 +5,36 @@
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-	  browsers: ['ChromeHeadless'],
-	  singleRun: true
-  });
+	config.set({
+		basePath: '',
+		frameworks: ['jasmine', '@angular-devkit/build-angular'],
+		plugins: [
+			require('karma-jasmine'),
+			require('karma-chrome-launcher'),
+			require('karma-jasmine-html-reporter'),
+			require('karma-coverage-istanbul-reporter'),
+			require('karma-junit-reporter'),
+			require('@angular-devkit/build-angular/plugins/karma')
+		],
+		client: {
+			clearContext: false // leave Jasmine Spec Runner output visible in browser
+		},
+		coverageIstanbulReporter: {
+			dir: require('path').join(__dirname, '../reports/coverage'),
+			reports: ['html', 'lcovonly'],
+			fixWebpackSourcePaths: true
+		},
+		reporters: ['progress', 'junit'],
+		junitReporter: {
+			outputDir: '../reports/junit',
+			outputFile: 'report.xml',
+			useBrowserName: false,
+		},
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
+		autoWatch: true,
+		browsers: ['ChromeHeadless'],
+		singleRun: true
+	});
 };
